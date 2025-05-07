9a3cc98c1492e884a92e6fa15a860b7686ffe18d
Product Migrator <genbee@example.com>
2025-05-07 00:20:07 +0000

    Migration result task_id 4ae4e0d9-d47f-4765-b7b9-7d57b48ae6e9

--- src/main/java/hello/controller/HelloController.java
+++ src/main/java/hello/controller/HelloController.java
@@ -4,6 +4,7 @@ import hello.declaration.TimeClient;
 import hello.model.SimpleTimeClient;
 import hello.model.Topic;
 import hello.service.TopicService;
+import hello.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,6 @@ import java.util.stream.Collectors;
 @RestController
 public class HelloController {
 
-
     String joinTemplate = "Joining All String ID's with JOIN method: ";
     String makeDistinctAndSortCharactersTemplate = "-------------Get all ID characters, select distict and sort with ID=   ";
     String splitAllIdWithColonSelectIDWithJavaKeywordThenSortThenJoinTemplate = "-------------Split All Id With Colon," +
@@ -34,9 +34,11 @@ public class HelloController {
     String findParticularFileInPathAndSortWithWalkFunctionTemplate = "----------Find File in present directory which strats with \"grad\",provided maximum depth=25 and sort :  with walk function";
     String readFileWithStreamFunctionTemplate = "---------Read \"temp.txt\" file with stream functions, having \"print\" witin it:  ";
 
-
     @Autowired
     private TopicService topicService;
+
+    @Autowired
+    private HelloService helloService;
 
     /**
      * Java 8 Date Time example
@@ -56,7 +58,6 @@ public class HelloController {
                 "Time in California: " + myTimeClient.getZonedDateTime("Canada/Central").toString();
 
     }
-
 
     /**
      * String Operations in Java 8
@@ -79,7 +80,6 @@ public class HelloController {
 
     }
 
-
     /**
      * File Operation in Java 8
      * @return
@@ -96,6 +96,10 @@ public class HelloController {
                 + readFileWithStreamFunctionTemplate + readFileWithStreamFunction;
     }
 
-
+    @RequestMapping("/hello")
+    public String method_a() {
+        String response = helloService.method_a();
+        return response;
+    }
 
 }
