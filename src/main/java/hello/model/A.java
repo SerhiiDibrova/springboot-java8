b52452bda25d71350d1292765fd8dd6ca2a8b2a1
Product Migrator <genbee@example.com>
2025-06-23 08:47:15 +0000

    Migration result task_id bea67a34-7546-4760-be07-a9a36f0d110d

---
+++ src/main/java/hello/model/A.java
@@ -0,0 +1,13 @@
+package hello.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class A {
+    private static final Logger logger = LoggerFactory.getLogger(A.class);
+
+    public String methodA() {
+        logger.info("Printing message from methodA");
+        return "A";
+    }
+}
